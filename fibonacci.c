144ceffb5de7205934b1590004c5206e0c949977
Utkarsh Gupta <utkarshgupta7714@gmail.com>
2024-10-24 13:42:37 +0530

    Created a fibonacci Program

---
+++ fibonacci.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int fib(int n) {
+    if(n ==0) {
+        return 0;
+    } else if (n ==1) {
+        return 1;
+    } else if (n < 0) {
+        return 0;
+    } else {
+        return n * fib(n-1);
+    }
+}
+
+int main() {
+    int number,output;
+    printf("Enter a number: ");
+    scanf("%d",&number);
+    output=fib(number);
+    printf("%d",output);
+    return 0;
+}
